ee4f86e6db6e6b77dec8cccb48d3d876b3182410
Stephan Baerwolf <stephan.baerwolf@tu-ilmenau.de>
2011-08-23 15:10:52 +0000

    sched: fix "check_preempt_tick(...)" by using "wakeup_preempt_entity(...)"
    Signed-off-by: Stephan Baerwolf <stephan.baerwolf@tu-ilmenau.de>

--- kernel/sched_fair.c
+++ kernel/sched_fair.c
@@ -1200,6 +1200,9 @@ dequeue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 	update_cfs_shares(cfs_rq);
 }
 
+static int
+wakeup_preempt_entity(struct sched_entity *curr, struct sched_entity *se);
+
 /*
  * Preempt the current task with a newly woken task if needed:
  */
@@ -1208,7 +1211,6 @@ check_preempt_tick(struct cfs_rq *cfs_rq, struct sched_entity *curr)
 {
 	unsigned long ideal_runtime, delta_exec;
 	struct sched_entity *se;
-	s64 delta;
 
 	delta_exec = curr->sum_exec_runtime - curr->prev_sum_exec_runtime;
 	
@@ -1237,12 +1239,7 @@ check_preempt_tick(struct cfs_rq *cfs_rq, struct sched_entity *curr)
 		return;
 
 	se = __pick_first_entity(cfs_rq);
-	delta = curr->vruntime - se->vruntime;
-
-	if (delta < 0)
-		return;
-
-	if (delta > ideal_runtime)
+	if (wakeup_preempt_entity(curr, se) > 0)
 		resched_task(rq_of(cfs_rq)->curr);
 }
 
@@ -1275,9 +1272,6 @@ set_next_entity(struct cfs_rq *cfs_rq, struct sched_entity *se)
 #endif
 	se->prev_sum_exec_runtime = se->sum_exec_runtime;
 }
-
-static int
-wakeup_preempt_entity(struct sched_entity *curr, struct sched_entity *se);
 
 /*
  * Pick the next process, keeping these things in mind, in this order:
